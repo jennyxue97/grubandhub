007d651a16640af1e617029f10ddb4680a60bca5
Binh Le <binhle@mit.edu>
2018-04-22 12:32:41 -0400

    added hover colors to course progress headers

--- course.js
+++ course.js
@@ -9,13 +9,13 @@ window.onload = function () {
 function askQuestion(){
     var question = document.createElement('div');
     question.className = "chat-question";
-    question.innerHTML = "Question: <br>" + document.getElementById("chat-input").value;
+    question.innerHTML = document.getElementById("chat-input").value;
     document.getElementById("chat-text").appendChild(question);
     document.getElementById("chat-input").value = "";
     setTimeout(function () {
         var answer = document.createElement('div');
         answer.className = "chat-answer";
-        answer.innerHTML = "Answer: <br> Sorry, the chat service is currently unavailable, please check back in a few minutes.";
+        answer.innerHTML = "Sorry, the chat service is currently unavailable, please check back in a few minutes.";
         document.getElementById("chat-text").appendChild(answer);
     }, 1000);
 }
